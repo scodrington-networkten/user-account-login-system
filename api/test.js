55d69af627fd68da5f176b57695e877d2fe0018d
simoncodrington <scodrington@networkten.com.au>
2025-05-13 16:03:02 +1000

    updated the serverless function

--- api/test.js
+++ api/test.js
@@ -1,3 +1,13 @@
-export default function handler(request, response) {
+export default function test(request, response) {
+
+
+    //console.log('METHOD:', request.method);
+    //console.log('QUERY:', request.query.test);
+   // console.log("WOO");
+
+    let message = request.query.message != undefined ? request.query.message : '';
+
+    console.log(`The Message is: ${message}`);
+
     response.status(200).json({ message: 'Hello from test function!' });
 }
