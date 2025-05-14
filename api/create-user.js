7814476b092f0813d66b78d9cdb6d5859979b0fb
simoncodrington <scodrington@networkten.com.au>
2025-05-14 13:50:48 +1000

    Lifted up the env file to be a top level one as it had issues being in a sub-directory. will use single .env for project + API

--- api/create-user.js
+++ api/create-user.js
@@ -1,3 +1,24 @@
-export default function createUser(request, response) {
-    return response.status(200).json({message: "user create"})
+
+import dotenv from 'dotenv';
+
+import {Client} from "pg";
+
+export default async function createUser(request, response) {
+
+    const result = dotenv.config();
+   // console.log(result);
+    //console.log(process.env);
+
+   // return response.status(200).json({message: "hello"});
+
+    const client = new Client();
+    try {
+        await client.connect();
+        const result = await client.query('SELECT NOW()');
+        await client.end();
+        return response.status(200).json({ now: result.rows[0].now });
+    } catch (error) {
+        return response.status(500).json({ error: error.message });
+    }
+
 }
