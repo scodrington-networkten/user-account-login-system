4c118f4f841f9ae7efe1a19e4ada83e06a841d36
simoncodrington <scodrington@networkten.com.au>
2025-07-21 10:13:22 +1000

    Footer work

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -13,13 +13,13 @@ const Footer = () => {
 
     useEffect(() => {
 
+        if (!genres) return;
+
         if (genres.length > 0) {
-            setTopGenres(genres.slice(0, 10));
+            setTopGenres(genres.slice(0, 5));
         } else {
             setTopGenres([])
         }
-
-
     }, [genres]);
 
 
@@ -40,20 +40,24 @@ const Footer = () => {
 
     return (
         <footer className=" bg-[var(--color-secondary)] text-gray-200 p-4 mt-4" id="page-footer">
-            <div className="genres">
-                <h3>Genres</h3>
-                {getGenres()}
+            <div className="inner">
+                <div className="footer-links genres">
+                    <h3>Genres</h3>
+                    {getGenres()}
+                </div>
+                <div className="footer-links important-links">
+                    <h3>Useful Links</h3>
+                    {getImportantLinks()}
+                </div>
+                <div className="footer-links popular-movies">
+                    <h3>Popular Movies</h3>
+                </div>
             </div>
-            <div className="important-links">
-                <h3>Important Links</h3>
-                {getImportantLinks()}
+            <div className="footer-bottom">
+                &copy; {new Date().getFullYear()} MovieSearch, powered by TheMovieDB <FontAwesomeIcon
+                icon={faVideo}/>
             </div>
-            <div className="popular-movies">
-                <h3>Popular Movies</h3>
-            </div>
-            <div className="container mx-auto text-center">
-                &copy; {new Date().getFullYear()} MovieSearch, powered by TheMovieDB <FontAwesomeIcon icon={faVideo}/>
-            </div>
+
         </footer>
     )
 }
